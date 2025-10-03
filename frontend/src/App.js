ef243991c802f41b30e06ca90f8bb7ca97fedcd2
Nethmini6 <164535968+Nethmini6@users.noreply.github.com>
2025-10-03 17:54:29 +0530

    Modifyed app.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./Pages/Dashboard";
 import Employees from "./Pages/Employees";
+import Employees from "./Pages/Login";
+import Employees from "./Pages/Signup";
 import './App.css';
 
 function App() {
@@ -10,11 +12,14 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Login />} />
         <Route path="/employees" element={<Employees />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+                             
